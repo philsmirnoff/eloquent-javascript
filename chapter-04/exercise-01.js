f273c41f52845ada487229c4a63202373c0a2fd2
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-09-25 21:38:54 -0400

    Chapter 04 - Data Structures: Objects and Arrays, Exercise 01 - Sum of Range

---
+++ chapter-04/exercise-01.js
@@ -0,0 +1,36 @@
+/*
+  Chapter 04 - Data Structures: Objects and Arrays
+  Exercise 01 - The sum of a range
+*/
+
+const range = (start, end, step = 1) => {
+  const result = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i+= step) {
+      result.push(i);
+    }
+  } else if (step < 0) {
+    for (let i = start; i >= end; i += step) {
+      result.push(i)
+    }
+  } else if (start > end) {
+    return result;
+  }
+
+  return result;
+}
+
+const sum = (numbers) => {
+  let total = 0;
+
+  for (let number of numbers) {
+    total += number
+  }
+
+  return total
+}
+
+console.log(range(1, 10)); // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1)); // → [5, 4, 3, 2]
+console.log(sum(range(1, 10))); // → 55
